2fa944c6be5d7a530cfc17ffccd6ac58c6916b11
Francois Chastanet <francois.chastanet@crossknowledge.com>
2020-11-21 20:24:37 +0100

    install bats

---
+++ test-install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+BATS_VERSION=master
+CURRENT_DIR=$( cd "$( readlink -f "${BASH_SOURCE[0]%/*}" )" && pwd )
+
+[[ ! -f "${CURRENT_DIR}/vendor/bats/bin/bats" ]] && (
+    rm -Rf "${CURRENT_DIR}/vendor/bats-install" "${CURRENT_DIR}/vendor/bats"
+    git clone https://github.com/bats-core/bats-core.git "${CURRENT_DIR}/vendor/bats-install"
+    cd "${CURRENT_DIR}/vendor/bats-install" || exit 1
+    git checkout ${BATS_VERSION}
+    ./install.sh "${CURRENT_DIR}/vendor/bats"
+)
--- test.sh
+++ test.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/env bash
 
-BATS_VERSION=master
 CURRENT_DIR=$( cd "$( readlink -f "${BASH_SOURCE[0]%/*}" )" && pwd )
 
-[[ ! -f "${CURRENT_DIR}/vendor/bats/bin/bats" ]] && (
-    rm -Rf "${CURRENT_DIR}/vendor/bats-install" "${CURRENT_DIR}/vendor/bats"
-    git clone https://github.com/bats-core/bats-core.git "${CURRENT_DIR}/vendor/bats-install"
-    cd "${CURRENT_DIR}/vendor/bats-install" || exit 1
-    git checkout ${BATS_VERSION}
-    ./install.sh "${CURRENT_DIR}/vendor/bats"
-)
+if [ ! -d "${CURRENT_DIR}/vendor/bats" ]; then
+  (>&2 echo "please run ./test-install.sh")
+  exit 1
+fi
 
 #[[ ! -f "vendor/bats-support/load.bash" ]] && git clone https://github.com/ztombol/bats-support.git "${CURRENT_DIR}/vendor/bats-support"
 #[[ ! -f "vendor/bats-assert/load.bash" ]] && git clone https://github.com/ztombol/bats-assert.git "${CURRENT_DIR}/vendor/bats-assert"
