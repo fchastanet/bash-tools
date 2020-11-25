45de33c795c17a5014d0200572915dfcd770fffa
Francois Chastanet <francois.chastanet@crossknowledge.com>
2020-11-25 10:12:08 +0100

    updated install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,10 @@
 mkdir -p ~/.bash-tools && cp -R conf/. ~/.bash-tools
 sed -i -e "s@^BASH_TOOLS_FOLDER=.*@BASH_TOOLS_FOLDER=$(pwd)@g"  ~/.bash-tools/.env
 
-sudo apt update
-sudo apt install -y parallel
-# remove parallel nagware
-mkdir ~/.parallel
-touch ~/.parallel/will-cite
+if ! which parallel 2>/dev/null; then
+  sudo apt update
+  sudo apt install -y parallel
+  # remove parallel nagware
+  mkdir -p ~/.parallel
+  touch ~/.parallel/will-cite
+fi
