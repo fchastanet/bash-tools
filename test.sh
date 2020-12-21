6877cbc12cd32be089af0a354e0bff8d67c905b5
Francois Chastanet <francois.chastanet@crossknowledge.com>
2020-12-21 01:59:59 +0100

    docker ubuntu based bash

--- test.sh
+++ test.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/env bash
 
 CURRENT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
+DEBUG_ARGS=""
+DOCKER_DEBUG_ARGS=()
+set -x
+if [ "${DEBUG:-0}" = "1" ]; then
+  DEBUG_ARGS="--no-tempdir-cleanup"
+  mkdir -p tmp
+  DOCKER_DEBUG_ARGS=(-v "$(pwd)/tmp:/tmp")
+fi
 if [ "${IN_BASH_DOCKER:-}" != "You're in docker" ]; then
-  docker build --build-arg BASH_IMAGE=amd64/bash:4.4 -t git-amd64/bash:4.4 .docker
-  docker run -it --rm -v "$(pwd):/bash" --user "$(id -u):$(id -g)" git-amd64/bash:4.4 /bash/test.sh $@
+  docker build \
+    --build-arg "BASH_IMAGE=ubuntu:20.04" \
+    --build-arg BASH_TAR_VERSION="5.1" \
+    -f .docker/Dockerfile.ubuntu \
+    -t git-ubuntu:5.1 \
+    .docker
+  docker run -it --rm \
+    -v "$(pwd):/bash" \
+    "${DOCKER_DEBUG_ARGS[@]}" \
+    --user "$(id -u):$(id -g)" \
+    git-ubuntu:5.1 /bash/test.sh ${DEBUG_ARGS} $@
   exit 0
 fi
 (
