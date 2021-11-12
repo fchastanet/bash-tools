348a0cc6dbbdfb8ce15365132556546ba5986cdf
Francois Chastanet <fchastanet@wiley.com>
2021-11-12 17:26:15 +0100

    add debug

--- awkLint.sh
+++ awkLint.sh
@@ -6,10 +6,13 @@ set -o pipefail
 BASE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 (
+  set -x
   cd "${BASE_DIR}"
-  trap 'rm -f /tmp/awkLint.log' ERR EXIT
+  trap 'rm -f /tmp/awkLint.log || true' ERR EXIT
   find bin bash-framework conf .build -type f -name '*.awk' -exec sh -c 'awk --source "BEGIN { exit(0) } END { exit(0) }" --lint=no-ext -f "$1" < /dev/null' _ {} \; | tee /tmp/awkLint.log
-  grep -q 'fatal:' /tmp/awkLint.log
+  if grep -q 'fatal:' /tmp/awkLint.log; then
+    exit 1
+  fi
 )
 status=$?
 exit ${status}
