3f983e852620599d9d8aaaf3bd76cbdda19eb904
Francois Chastanet <fchastanet@wiley.com>
2021-11-12 19:04:13 +0100

    github actions linters

--- awkLint.sh
+++ awkLint.sh
@@ -6,7 +6,6 @@ set -o pipefail
 BASE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 (
-  set -x
   cd "${BASE_DIR}"
   trap 'rm -f /tmp/awkLint.log || true' ERR EXIT
   find bin bash-framework conf .build -type f -name '*.awk' -exec sh -c 'awk --source "BEGIN { exit(0) } END { exit(0) }" --lint=no-ext -f "$1" < /dev/null' _ {} \; | tee /tmp/awkLint.log
--- dockerLint.sh
+++ dockerLint.sh
@@ -6,15 +6,14 @@ set -o pipefail
 BASE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 if (( $# == 0 )); then
-  set -- --check-sourced -x -f checkstyle
+  set -- -f checkstyle
 fi
 
 (
+  set -x
   cd "${BASE_DIR}"
   # shellcheck disable=SC2046
-  LC_ALL=C.UTF-8 shellcheck "$@" \
-        $(find bin bash-framework conf .build -type f -regextype posix-egrep \
-        ! -regex '.*\.(env|log|sql|puml|awk)$' ! -name '.*')
+  hadolint "$@" $(find .docker -type f -name 'Dockerfile*')
 )
 status=$?
 exit ${status}
