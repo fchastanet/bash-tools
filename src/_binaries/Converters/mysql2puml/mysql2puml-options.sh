092bcb34c5a6768a1b2202b85dbce790d6a16a88
François Chastanet <fchastanet@wiley.com>
2024-08-26 22:25:59 +0200

    compiled mysql2puml using go compiler

---
+++ src/_binaries/Converters/mysql2puml/mysql2puml-options.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# shellcheck disable=SC2034
+declare copyrightBeginYear="2020"
+# shellcheck disable=SC2034
+declare versionNumber="1.0"
+
+declare optionBashFrameworkConfig="${BASH_TOOLS_ROOT_DIR}/.framework-config"
+
+optionHelpCallback() {
+  mysql2pumlCommandHelp
+  exit 0
+}
+
+longDescriptionFunction() {
+  echo -e "${__HELP_TITLE}EXAMPLE 1:${__HELP_NORMAL}"
+  echo -e "${__HELP_EXAMPLE}mysql2puml dump.dql${__HELP_NORMAL}"
+  echo
+  echo -e "${__HELP_TITLE}EXAMPLE 2:${__HELP_NORMAL}"
+  echo -e "${__HELP_EXAMPLE}mysqldump --skip-add-drop-table \\"
+  echo -e "  --skip-add-locks \\"
+  echo -e "  --skip-disable-keys \\"
+  echo -e "  --skip-set-charset \\"
+  echo -e "  --user=root \\"
+  echo -e "  --password=root \\"
+  echo -e "  --no-data skills | mysql2puml"
+  echo -e "${__HELP_NORMAL}"
+  echo -e "${__HELP_TITLE}LIST OF AVAILABLE SKINS:${__HELP_NORMAL}"
+  Conf::getMergedList "mysql2pumlSkins" ".puml" "  - "
+}
+
+optionSkinCallback() {
+  declare -a skinList
+  readarray -t skinList < <(Conf::getMergedList "mysql2pumlSkins" ".puml" "")
+  # shellcheck disable=SC2154
+  if ! Array::contains "${optionSkin}" "${skinList[@]}"; then
+    Log::displayError "${SCRIPT_NAME} - invalid skin '${optionSkin}' provided"
+    exit 1
+  fi
+}
+
+inputSqlFileCallback() {
+  # shellcheck disable=SC2154
+  if [[ ! -f "${inputSqlFile}" ]]; then
+    Log::displayError "${SCRIPT_NAME} - File '${inputSqlFile}' does not exists"
+    exit 1
+  fi
+}
